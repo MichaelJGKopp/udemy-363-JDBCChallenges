de6656f5a05f58aef10b2b5c15e65e323306c21f
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-10-27 22:09:39 +0100

    set up MysqlDataSource

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -2,33 +2,16 @@ package dev.lpa;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-
 public class Main {
+  
+  private static String USE_SCHEMA = "USE storefront"; // DDL statement to set database
   
   public static void main(String[] args) {
     
     var dataSource = new MysqlDataSource();
     dataSource.setServerName("localhost");
     dataSource.setPort(3306);
-    dataSource.setDatabaseName("music");
-    try {
-      dataSource.setMaxRows(10);
-    } catch (SQLException e) {
-      throw new RuntimeException(e);
-    }
-    
-    try (Connection connection = dataSource.getConnection(
-      System.getenv("MYSQL_USER"), System.getenv("MYSQL_PASS"));
-         var statement = connection.createStatement()) {
-        var result = statement.executeQuery("SELECT * FROM music.albumview");
-        while (result.next()) {
-          String data = result.getString(1);
-          System.out.println(data);
-        }
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
+    dataSource.setUser(System.getenv("MYSQL_USER"));
+    dataSource.setPassword(System.getenv("MYSQL_PASS"));
   }
 }
