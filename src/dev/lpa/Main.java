08f5f2f2c56abc9a153fa8dfe274b72482845db2
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-10-27 22:16:07 +0100

    add method checkSchema

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -2,6 +2,11 @@ package dev.lpa;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
 public class Main {
   
   private static String USE_SCHEMA = "USE storefront"; // DDL statement to set database
@@ -13,5 +18,16 @@ public class Main {
     dataSource.setPort(3306);
     dataSource.setUser(System.getenv("MYSQL_USER"));
     dataSource.setPassword(System.getenv("MYSQL_PASS"));
+  }
+  
+  private static boolean checkSchema(Connection conn) {
+    
+    try (Statement statement = conn.createStatement()) {
+      statement.execute(USE_SCHEMA);
+    } catch (SQLException e) {
+      e.printStackTrace();
+      return false;
+    }
+    return true;
   }
 }
