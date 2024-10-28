8ad46df65fd95d05e048a410f2eeea51d97865d7
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-10-28 20:21:45 +0100

    solve challenge

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -2,10 +2,10 @@ package dev.lpa;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
 
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 
 public class Main {
@@ -22,6 +22,8 @@ public class Main {
     dataSource.setUser(System.getenv("MYSQL_USER"));
     dataSource.setPassword(System.getenv("MYSQL_PASS"));
     
+    Scanner scanner = new Scanner(System.in);
+    
     try (Connection conn = dataSource.getConnection()) {
       
       DatabaseMetaData metaData = conn.getMetaData();
@@ -29,6 +31,11 @@ public class Main {
       if (!checkSchema(conn)) {
         System.out.println("storefront schema does not exist");
         setUpSchema(conn);
+      } else {
+        insertOrder(conn, "rareItem");
+        System.out.println("Enter order id to delete: ");
+        deleteOrder(conn, scanner.nextInt());
+        printOrder(conn);
       }
     } catch (SQLException e) {
       throw new RuntimeException(e);
@@ -86,6 +93,76 @@ public class Main {
       }
     } catch (SQLException e) {
       e.printStackTrace();
+    }
+  }
+  
+  private static void insertOrder(Connection conn, String itemDescription) throws SQLException {
+    
+    int orderId = -1;
+    try (Statement statement = conn.createStatement()) {
+      conn.setAutoCommit(false);
+      
+      // Insert entry into order table and get key orderId
+      DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+      var now = LocalDateTime.now().format(dtf);
+      String insertOrder = "INSERT INTO storefront.order (order_date) VALUES (%s)"
+                             .formatted(statement.enquoteLiteral(now));
+      System.out.println(insertOrder);
+      
+      statement.execute(insertOrder, Statement.RETURN_GENERATED_KEYS);
+      ResultSet rs = statement.getGeneratedKeys();
+      orderId = (rs != null && rs.next()) ? rs.getInt(1) : -1;
+      System.out.println("Generated order ID = " + orderId);
+      
+      // Insert entry into order table and get key orderId
+      String insertOrderDetails =
+        "INSERT INTO storefront.order_details (item_description, order_id) VALUES (%s, %d)"
+                             .formatted(statement.enquoteLiteral(itemDescription), orderId);
+      System.out.println(insertOrderDetails);
+      
+      statement.execute(insertOrderDetails);
+      conn.commit();
+    } catch (SQLException e) {
+      conn.rollback();
+      e.printStackTrace();
+      System.err.println("SQLException during transaction, rollback changes: " + e.getMessage());;
+    }
+    conn.setAutoCommit(true);
+    printOrder(conn);
+  }
+  
+  private static void printOrder(Connection conn) throws SQLException {
+    
+    try (Statement statement = conn.createStatement()) {
+      System.out.println("===============================");
+      String query = "SELECT * FROM storefront.order_summary";
+      ResultSet rs = statement.executeQuery(query);
+      ResultSetMetaData metaData =  rs.getMetaData();
+      int columnCount = metaData.getColumnCount();
+      
+      for (int i = 1; i <= columnCount; i++) {
+        System.out.printf("%-20s", metaData.getColumnLabel(i).toUpperCase());
+      }
+      System.out.println();
+      while(rs.next()) {
+        for (int i = 1; i <= columnCount; i++) {
+          System.out.printf("%-20s", rs.getString(i));
+        }
+        System.out.println();
+      }
+    }
+  }
+  
+  private static boolean deleteOrder(Connection conn, int orderId) {
+    
+    try (Statement statement = conn.createStatement()) {
+      String deleteOrder = "DELETE FROM storefront.order WHERE order_id=%d"
+                             .formatted(orderId);
+      statement.execute(deleteOrder);
+      return statement.getUpdateCount() > 1;
+    } catch (SQLException e) {
+      e.printStackTrace();
+      return false;
     }
   }
 }
